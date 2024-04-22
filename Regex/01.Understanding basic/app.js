713468b49fb187b089e6476b770a260ea2a65e6a
Rampraveen <rampraveen018@gmail.com>
2024-04-22 13:28:16 +0530

    Starting with meta characters

--- Regex/01.Understanding basic/app.js
+++ Regex/01.Understanding basic/app.js
@@ -3,11 +3,11 @@ let txt = "Programming courses always starts with a hello world example."
 
 
 //  Regex Syntax
-let regex1 = new RegExp("hello");
+let regex1 = new RegExp(/hello/);
 let regex2 = /world/;  
 
 
-
+//  Basic Syntax
 console.log(regex1.exec(txt));
 
 console.log(regex2.text(txt)); // Return Boolean whether it match or not
@@ -19,3 +19,16 @@ console.log(txt.match(regex2)); // return an array of regex, index & inputs if i
 
 console.log(txt.search(regex2)); // return the index of the string in the text or return -1
 console.log(txt.replace(regex2,'Bhummi')); // replace the the search value by replace value
+
+//  Flags
+
+//  /pattern/flags or new Regex('pattern','flags')
+// g- global match check for all occurence 
+// i - case insensitive match, case doen't matter
+// m - multi-line match, check the first and last character of the string effective with(^ - start) or ($ - end) 
+let regex3= 
+console.log(txt.match())
+
+
+// META-CHARACTERS 
+//  ^$.*+?=!:|\/()[]{}
